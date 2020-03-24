b8ea4ff2aa9e889c1dbadde925cb6b72f43b98c7
Schm1tz1 <ro-man@web.de>
2020-03-24 19:18:50 +0100

    Fixed vulnerability due to usage of Log4j, fixed pom

--- src/main/java/com/github/schm1tz1/kafka/serde/JsonSimpleDeserializer.java
+++ src/main/java/com/github/schm1tz1/kafka/serde/JsonSimpleDeserializer.java
@@ -1,14 +1,15 @@
 package com.github.schm1tz1.kafka.serde;
 
 import org.apache.kafka.common.serialization.Deserializer;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.util.Map;
 
 public class JsonSimpleDeserializer implements Deserializer<JSONObject> {
-    private static final Logger logger = Logger.getRootLogger();
+    private static final Logger logger = LogManager.getRootLogger();
     private String encoding = "UTF8";
     private JSONParser jsonParser = null;
 
@@ -18,8 +19,9 @@ public class JsonSimpleDeserializer implements Deserializer<JSONObject> {
 
     /**
      * Configure this class.
+     *
      * @param configs configs in key/value pairs
-     * @param isKey whether is for key or value
+     * @param isKey   whether is for key or value
      */
     public void configure(Map<String, ?> configs, boolean isKey) {
 
@@ -27,8 +29,9 @@ public class JsonSimpleDeserializer implements Deserializer<JSONObject> {
 
     /**
      * Deserialize a record value from a byte array into a JSOnObject.
+     *
      * @param topic topic associated with the data
-     * @param data serialized bytes; may be null; implementations are recommended to handle null by returning a value or null rather than throwing an exception.
+     * @param data  serialized bytes; may be null; implementations are recommended to handle null by returning a value or null rather than throwing an exception.
      * @return JSONObject
      */
     public JSONObject deserialize(String topic, byte[] data) {
--- src/main/java/com/github/schm1tz1/kafka/serde/JsonSimpleSerializer.java
+++ src/main/java/com/github/schm1tz1/kafka/serde/JsonSimpleSerializer.java
@@ -1,19 +1,21 @@
 package com.github.schm1tz1.kafka.serde;
 
 import org.apache.kafka.common.serialization.Serializer;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 
 import java.util.Map;
 
 public class JsonSimpleSerializer implements Serializer<JSONObject> {
 
-    private static final Logger logger = Logger.getRootLogger();
+    private static final Logger logger = LogManager.getRootLogger();
 
     /**
      * Configure this class.
+     *
      * @param configs configs in key/value pairs
-     * @param isKey whether is for key or value
+     * @param isKey   whether is for key or value
      */
     public void configure(Map<String, ?> configs, boolean isKey) {
 
@@ -23,7 +25,7 @@ public class JsonSimpleSerializer implements Serializer<JSONObject> {
      * Convert JSONObject into a byte array.
      *
      * @param topic topic associated with data
-     * @param data JSONObject
+     * @param data  JSONObject
      * @return serialized bytes
      */
     public byte[] serialize(String topic, JSONObject data) {
@@ -41,7 +43,7 @@ public class JsonSimpleSerializer implements Serializer<JSONObject> {
 
     /**
      * Close this serializer.
-     *
+     * <p>
      * This method must be idempotent as it may be called multiple times.
      */
     public void close() {
